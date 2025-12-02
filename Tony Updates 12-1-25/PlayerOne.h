7016914f8a349c02f1873b85b292e049176a3b06
Anthony Alperov <talperov10@gmail.com>
2025-12-01 20:16:28 -0800

    Update PlayerOne.h

--- Tony Updates 12-1-25/PlayerOne.h
+++ Tony Updates 12-1-25/PlayerOne.h
@@ -1,29 +1,28 @@
 #pragma once
 #include "Character.hpp"
-#include "Animation.hpp"
 #include <map>
 #include <string>
 
 class PlayerOne : public Character
 {
 private:
+    bool facingRight;
     std::map<std::string, Animation> animations;
     std::string currentAction;
-    bool facingRight;  // Track direction
+
+    void updateHitBox();
 
 public:
     PlayerOne();
 
-    //MOVEMENT
     void moveLeft() override;
     void moveRight() override;
-  
+    void moveJump() override;
 
-    //COMBAT
     void attack() override;
     void takeDamage(int damage) override;
 
-    //ANIMATION
-    void update(float deltaTime);   // Update current animation
-    sf::Sprite getSprite() const;   // Get current sprite to draw
+    void update(float deltaTime) override;
+    sf::Sprite getSprite() const;
 };
+
